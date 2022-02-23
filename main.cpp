c9dbc46744e5f0b9dff52c9bdca44621f6fcb1a8
renhaofan <yqykrhf@163.com>
2022-02-23 19:33:15 +0800

    remove chinese comment

--- main.cpp
+++ main.cpp
@@ -5,53 +5,28 @@
 #include <QDebug>
 #include <QMessageBox>
 
-
-//int main(int argc, char *argv[]) {
-//    QApplication a(argc, argv);
-//    ImageViewer w;
-//    // reference: https://www.cxymm.net/article/qq_36170958/111300170
-//    QSharedMemory shared("share"); //绑定共享内存
-//    if (shared.attach()) {
-////        QMessageBox::warning(NULL, "Warning", "One instance of application has already running in tray mode");
-//        qWarning() << "Can't start more than one instance of the application.";
-//        exit(0);
-//    } else {
-//        qDebug() << "Application started successfully.";
-//    }
-//    shared.create(1); // 1 byte
-//    w.show();
-
-//    return a.exec();
-//}
-
-
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     ImageViewer w;
 
-    //确保只运行一次
+    // make sure one instance
     QSystemSemaphore sema("SystemSemaphore", 1, QSystemSemaphore::Open);
-    sema.acquire();//在临界区操作共享内存  SharedMemory
+    sema.acquire(); // operate SharedMemory in temporary region
 
-    QSharedMemory mem("SharedObject"); //全局对象名
+    QSharedMemory mem("SharedObject"); // global object name
     bool bCreate = mem.create(1);
     qDebug() << "bCreate=============" << bCreate;
 
-    if(bCreate)
-    {
+    if (bCreate) {
         qDebug() << "create shared memory success======";
         w.show();
-    }
-    else
-    {
+    } else {
         QMessageBox::warning(NULL, "Warning", "One instance of application has already running in tray mode");
         qDebug() << "An instance has already been running======";
-        sema.release(); //如果是 Unix 系统，会自动释放。
+        sema.release() ; // unix system, release automatically
         exit(0);
     }
 
-    sema.release();//临界区
-
+    sema.release(); // temporary region
     return a.exec();
 }
